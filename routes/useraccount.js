fd7dcbf8d8224d51fddd5292ff1b1b22d8e2dafd
klaramd <die_km@hotmail.de>
2018-05-23 15:45:51 +0200

    ID Security Issue fixed

--- routes/useraccount.js
+++ routes/useraccount.js
@@ -15,50 +15,48 @@ router.get('/', async function(req, res, next) {
 //Getting new trainings input
 router.post('/submittraining', async function(req,res,next){
     //check if their is an ID in the session
-    //var currentID = req.params.id;
-    await dbPromise.Training.create({
-        userId: req.session.userId,
-        startDate: req.body.startdate,
-        startTime: req.body.starttime,
-        endDate: req.body.enddate,
-        endTime: req.body.endtime,
-        description: req.body.details
-    })
-    /* DISPLAYING SHIZZLE
-     const trainingtest = await dbPromise.User.findById(2, {include: [dbPromise.Training]})
-     console.log(trainingtest.trainings.map(t => t.description))
-     */
-    res.redirect('/useraccount/' );
+    if(req.session.userId == null){
+        res.render('nicetry');
+    } else {
+        //var currentID = req.params.id;
+        await
+        dbPromise.Training.create({
+            userId: req.session.userId,
+            startDate: req.body.startdate,
+            startTime: req.body.starttime,
+            endDate: req.body.enddate,
+            endTime: req.body.endtime,
+            description: req.body.details
+        })
+        res.redirect('/useraccount/')
+    }
 });
 
 //Getting new weight input
 router.post('/submitweight', async function(req,res,next){
-    // var currentID = req.params.id;
-    await dbPromise.Weight.create({
-        userId: req.session.userId,
-        weightDate: req.body.weightdate,
-        weightKG: req.body.weightkg
-    })
+    if(req.session.userId == null){
+        res.render('nicetry');
+    } else {
+        // var currentID = req.params.id;
+        await dbPromise.Weight.create({
+            userId: req.session.userId,
+            weightDate: req.body.weightdate,
+            weightKG: req.body.weightkg
+        })
 
-    /*DISPLAYING SHIZZLE
-     const test = await dbPromise.User.findById(2, {include: [dbPromise.Weight] })
-     //console.log(test.weights)
-     console.log(test.weights.map(w => w.weightKG))
-     */
-    res.redirect('/useraccount/');
+        res.redirect('/useraccount/');
+    }
 });
 
 
 
 //Logout
-//Can't destroy it since action thingy works only with forms
 router.post('/logout', function(req,res,next){
     req.session.destroy(function(err) {
         if(err) {
             console.log(err);
         } else {
             res.redirect('/');
-            // req.session.isLoggedIn = false;
         }
     });
 })
